d751d183845be12447f557075d000b7da76e516c
Adrian Krasowski <Adr.Krasowski@gmail.com>
2024-04-30 22:15:57 +0200

    Add DomesticShipmentFactory Tests

---
+++ DesignPatternsTests/Creational/AbstractFactory/Factories/Delivery/DomesticShipmentFactoryTest.cs
@@ -0,0 +1,46 @@
+﻿using DesignPatternsExamples.Creational.AbstractFactory.Factories.Delivery;
+using DesignPatternsExamples.Creational.AbstractFactory.Factories.Delivery.Abstract;
+using DesignPatternsExamples.Creational.AbstractFactory.Models.DomesticShipment;
+using Xunit;
+
+namespace DesignPatternsTests.Creational.AbstractFactory.Factories.Delivery
+{
+    public class DomesticShipmentFactoryTest
+    {
+        //Arrange
+        private IDeliveryFactory _uut = new DomesticShipmentFactory();
+        
+        [Fact]
+        public void DomesticShipmentyFactoryShouldReturnDomesticDeliveryRates()
+        {
+
+            //Act
+            var actualDeliveryRates = _uut.GetDeliveryRates();
+
+            //Assert
+            Assert.IsType<DomesticDeliveryRates>(actualDeliveryRates);
+        }
+
+        [Fact]
+        public void DomesticShipmentyFactoryShouldReturnDomesticShipmentSpecification()
+        {
+
+            //Act
+            var actualDeliveryRates = _uut.GetShipmentSpecification();
+
+            //Assert
+            Assert.IsType<DomesticShipmentSpecification>(actualDeliveryRates);
+        }
+
+        [Fact]
+        public void DomesticShipmentyFactoryShouldReturnDomesticDeliverySpecification()
+        {
+
+            //Act
+            var actualDeliveryRates = _uut.GetDeliverySpecification();
+
+            //Assert
+            Assert.IsType<DomesticDeliverySpecification>(actualDeliveryRates);
+        }
+    }
+}
