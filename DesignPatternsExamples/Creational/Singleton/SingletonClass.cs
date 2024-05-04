e8afdd9ce0fbd716dfa1a217d77bd30eb6dcaf3b
Adrian Krasowski <Adr.Krasowski@gmail.com>
2024-05-04 21:05:32 +0200

    Feature: Singleton - Add design pattern example

---
+++ DesignPatternsExamples/Creational/Singleton/SingletonClass.cs
@@ -0,0 +1,18 @@
+﻿namespace DesignPatternsExamples.Creational.Singleton
+{
+    public class SingletonClass
+    {
+        private SingletonClass _instance = null;
+
+        private SingletonClass()
+        {
+        }
+
+        public SingletonClass GetInstance() 
+        {
+            if(_instance == null)
+                _instance = new SingletonClass();
+            return _instance;
+        }
+    }
+}
